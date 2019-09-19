9173b6c15fe7e9145ab2c12a540a8d85299a40a0
Hendrik Hofstadt <hendrik@certus.one>
2019-09-19 21:07:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+##Tendermint block miss exporter
+
+This prometheus exports watches a tendermint node and counts the blocks missed by a specific validator.
+
+The `tm_mon_misses` starts from zero and is incremented every time the specified validator misses a block.
+
+In order to track whether the exporter is not stuck it also exposes the last processed height as `tm_mon_height`
+
+###Configuration
+
+The configuration needs to be passed in via the environment.
+
+| Name | Description |
+|------|-------------|
+| LADDR | Listening address (e.g. `:8080`)
+| RPC | RPC address of the tendermint node to use for monitoring |
+| ADDRESS | The hex consensus address of the validator that should be monitored |
+
+### Error handling
+
+In case of errors during startup the program will panic. Errors during runtime are printed to the console and might
+lead to the exporter not processing blocks. This will be visible in prometheus as `tm_mon_height` will stop increasing.
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"encoding/hex"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/tendermint/tendermint/rpc/client"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var (
+	valAddrHex = os.Getenv("ADDRESS")
+	rpcHost    = os.Getenv("RPC")
+	lAddr      = os.Getenv("LADDR")
+)
+
+func main() {
+	go pollRoutine()
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	panic(http.ListenAndServe(lAddr, nil))
+}
+
+func pollRoutine() {
+	c := client.NewHTTP(rpcHost, "/ws")
+	missCounter := prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace:   "tm",
+		Subsystem:   "mon",
+		Name:        "misses",
+		Help:        "Amount of missed blocks since exporter start",
+		ConstLabels: nil,
+	})
+	prometheus.MustRegister(missCounter)
+
+	heightGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace:   "tm",
+		Subsystem:   "mon",
+		Name:        "height",
+		Help:        "Current block height that the monitor has processed",
+		ConstLabels: nil,
+	})
+	prometheus.MustRegister(heightGauge)
+
+	validatorAddress, err := hex.DecodeString(valAddrHex)
+	if err != nil {
+		panic(err)
+	}
+
+	var lastHeight int64
+OUTER:
+	for {
+		select {
+		case <-time.Tick(time.Second):
+			res, err := c.Block(nil)
+			if err != nil {
+				log.Printf("block could not be requested; err=%v\n", err)
+				continue
+			}
+
+			if res.Block.Height > lastHeight {
+				lastHeight = res.Block.Height
+				heightGauge.Set(float64(lastHeight))
+
+				for _, pre := range res.Block.LastCommit.Precommits {
+					if pre != nil && bytes.Equal(validatorAddress, pre.ValidatorAddress) && !pre.BlockID.IsZero() {
+						// We signed and the vote is not zero (nil votes also count as downtime)
+						continue OUTER
+					}
+				}
+
+				// Seems like we missed the block
+				missCounter.Inc()
+			}
+		}
+	}
+}
